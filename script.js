668c071bb1899bcccaeb24846390a914835fd5f8
Eliott G1~ <eliott.giraud@hotmail.fr>
2023-12-27 16:41:28 +0100

    test preloader

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    // Masquer l'écran de préchargement une fois que la page est entièrement chargée
+    var preloadScreen = document.querySelector(".preload-screen");
+    preloadScreen.style.display = "none";
+};
